5aee45b407b15ca3090ef234bb6cc04208d1adf2
eavae <384126461@qq.com>
2018-05-10 16:14:05 +0800

    Wrap native select as a SelectField.

---
+++ src/component/selectField.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {omit} from 'ramda'
+
+import withField, {fieldPropTypes} from '../hoc/withField'
+
+const Select = (props) => <select {...omit(['error', 'clearError', 'children'], props)}>{props.children}</select>
+
+Select.propTypes = {
+  ...fieldPropTypes,
+  children: PropTypes.element,
+}
+
+Select.defaultProps = {
+  children: null,
+}
+
+export default withField({
+  parse: (e) => e.target.value,
+})(Select)
