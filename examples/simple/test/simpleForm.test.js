7eeb87a7c29fe48ebf08200401049d924d75ca41
eavae <384126461@qq.com>
2018-07-05 16:57:45 +0800

    Add e2e test to simple form example.

---
+++ examples/simple/test/simpleForm.test.js
@@ -0,0 +1,50 @@
+const Nightmare = require('nightmare')
+const chai = require('chai')
+const expect = chai.expect
+
+describe('simple form', function() {
+  this.timeout('10s')
+
+  let nightmare;
+
+  before(function() {
+    nightmare = Nightmare()
+  });
+
+  it('should sync the values to the store', function(done) {
+    nightmare
+      .goto('http://localhost:3000/')
+      .type('input[name="firstName"]', 'John')
+      .type('input[name="lastName"]', 'Snow')
+      .type('input[name="email"]', 'john.snow@hbo.com')
+      .check('input[name="sex"][value="male"]')
+      .select('select[name="favoriteColor"]', '00ff00')
+      .click('input[name="employed"]')
+      .type('textarea[name="notes"]', 'You know nothing!')
+      .evaluate(() => document.querySelector('code').innerText)
+      .then(outputJson => {
+        expect(JSON.parse(outputJson)).to.deep.equal({
+          firstName: 'John',
+          lastName: 'Snow',
+          email: 'john.snow@hbo.com',
+          favoriteColor: '00ff00',
+          employed: true,
+          notes: 'You know nothing!'
+        })
+        done()
+      })
+      .catch(done)
+  })
+
+  it('should clear the value when click Clear Values button', (done) => {
+    nightmare
+      .click('button[type="button"]')
+      .evaluate(() => document.querySelector('code').innerText)
+      .end()
+      .then(outputJson => {
+        expect(JSON.parse(outputJson)).to.deep.equal({})
+        done();
+      })
+      .catch(done)
+  });
+})
