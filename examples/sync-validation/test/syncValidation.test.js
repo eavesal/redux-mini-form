e577eaa480fafc958fef49654e6d116b49971e90
eavae <384126461@qq.com>
2018-07-06 14:57:28 +0800

    Add e2e test for sync validation form example.

---
+++ examples/sync-validation/test/syncValidation.test.js
@@ -0,0 +1,43 @@
+const Nightmare = require('nightmare')
+const {expect} = require('chai')
+
+describe('sync validation form', function() {
+  this.timeout('10s')
+
+  let nightmare;
+
+  before(function() {
+    nightmare = Nightmare()
+  })
+
+  it('should show required errors', function(done) {
+    nightmare
+      .goto('http://localhost:3000/')
+      .click('button[type="submit"]')
+      .evaluate(() => document.querySelectorAll('.required-error').length)
+      .then((requiredErrorCount) => {
+        expect(requiredErrorCount).to.equal(3)
+        done()
+      })
+      .catch(done)
+  })
+
+  it('should show field validation errors', function(done) {
+    nightmare
+      .type('input[name="username"]', 'some characters greater than 15')
+      .type('input[name="email"]', 'an invalid email')
+      .type('input[name="age"]', 24)
+      .click('button[type="submit"]')
+      .evaluate(() => Array.from(document.querySelectorAll('.field-error')).map((el) => el.innerText))
+      .end()
+      .then((errorMessages) => {
+        expect(errorMessages).to.deep.equal([
+          'Must be 15 characters or less',
+          'Invalid email address',
+          'Must be a number'
+        ])
+        done()
+      })
+      .catch(done)
+  })
+})
